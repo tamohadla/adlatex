1bb2c264a1730d6d4522be2632b45f7d07ed13a8
tamohadla <tamoh@live.com>
2026-01-05 21:07:25 +0200

    Update supabaseClient.js

--- shared/supabaseClient.js
+++ shared/supabaseClient.js
@@ -1,6 +1,6 @@
 // shared/supabaseClient.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
+export const BASE_PATH = "/adlatex";
 export const supabase = createClient(
   "https://nnzsnhnhfzhyumrowtyc.supabase.co",
   "sb_publishable_Ycve0SmNhf_5ScYvJ8vMFA_Ibk-XA6U"
