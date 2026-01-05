5c353eeb0b694c148147594e6b8a277cef8f1540
tamohadla <tamoh@live.com>
2026-01-05 20:53:53 +0200

    Create supabaseClient.js

---
+++ shared/supabaseClient.js
@@ -0,0 +1,7 @@
+// shared/supabaseClient.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+export const supabase = createClient(
+  "https://nnzsnhnhfzhyumrowtyc.supabase.co",
+  "sb_publishable_Ycve0SmNhf_5ScYvJ8vMFA_Ibk-XA6U"
+);
